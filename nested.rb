4da9eae229751fad926c5b303c50fa049e979be7
Nyaradzo Bere <nyaradzo.bere@colorado.edu>
2020-06-12 03:59:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -38,7 +38,7 @@ def alan_kay_is_known_for
       }
     }
 alan = programmer_hash[:alan_kay][:known_for]
-puts alan
+alan
 end
 
 
@@ -59,7 +59,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+dennis = programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
