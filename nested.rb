2a21bff992ef8fcd5c9b9eaac480d0a2646cc858
Nyaradzo Bere <nyaradzo.bere@colorado.edu>
2020-06-12 04:05:25 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,8 @@ def hopper
         :languages => ["C"]
       }
     }
-
+grace = programmer_hash[:grace_hopper]
+grace
 end
 
 def alan_kay_is_known_for
@@ -36,8 +37,11 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+alan = programmer_hash[:alan_kay][:known_for]
+alan
 end
+
+
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -55,7 +59,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+dennis = programmer_hash[:dennis_ritchie][:languages][0]
+dennis
 end
 
 def adding_matz
@@ -81,7 +86,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash[:yukihiro_matsumoto] = {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]
+}
+programmer_hash
 end
 
 def changing_alan
@@ -103,7 +112,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for] = "GUI"
+programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +135,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+programmer_hash
 end
